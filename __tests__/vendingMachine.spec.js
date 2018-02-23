a48d55421fc12f1cfc203ff9cb0909df49eb2374
Scott <scottdlivingstone@gmail.com>
2018-02-23 10:06:56 -0800

    Add restock tests and function

--- __tests__/vendingMachine.spec.js
+++ __tests__/vendingMachine.spec.js
@@ -6,12 +6,20 @@ const myMachine = new vendingMachine({ inventory, change });
 describe("vendingMachine", () => {
   describe("restock", () => {
     test("invalid item is passed to the function", () => {
-      const result = myMachine.restockItems({ item: a33, quantity: 1000 });
+      const result = () => {
+        myMachine.restockItem({ item: "a33", quantity: 1000 });
+      };
       expect(result).toThrowError();
     });
+    //initial a1 quantity is 5
     test("valid item is passed to the function", () => {
-      const result = myMachine.restockItems({ item: a1, quantity: 63 });
-      expect(result).toEqual(inventory.a1.quantity + 63);
+      const result = myMachine.restockItem({ item: "a1", quantity: 63 });
+      expect(result).toEqual(68);
+    });
+    //quantity should persist with the current instance of the vending machine
+    test("add more items to a previously restocked item", () => {
+      const result = myMachine.restockItem({ item: "a1", quantity: 2 });
+      expect(result).toEqual(70);
     });
   });
   describe("change", () => {});
